e0701669299466e480d684886ea890b9d80ca8e8
Francisco <fcocc77@gmail.com>
2024-08-17 17:46:45 -0400

    update_menu: update

--- src/update_menu.py
+++ src/update_menu.py
@@ -7,6 +7,7 @@ from functools import partial
 import re
 import nuke  # type: ignore
 
+from ..python_util.util import jprint
 from ..nuke_util.nuke_util import get_nuke_path
 from .connection import GET
 
@@ -21,15 +22,47 @@ def create_node(data):
     n.setName(name)
 
     # Knobs
-    for k, _input in data['input']['required'].items():
+    for k in data['input_order']['required']:
+        _input = data['input']['required'][k]
 
-        if _input[0] == 'INT':
+        _class = _input[0]
+        info = _input[1] if len(_input) == 2 else {}
+
+        tooltip = info.get('tooltip', '')
+        default_value = info.get('default', 0)
+
+        if _class == 'INT':
             knob = nuke.Int_Knob(k)
-            n.addKnob(knob)
+            knob.setDefaultValue([default_value])
+            knob.setTooltip(tooltip)
 
-        elif type(_input[0]) == list:
-            knob = nuke.Enumeration_Knob(k, k, _input[0])
-            n.addKnob(knob)
+        elif _class == 'FLOAT':
+            min_value = info.get('min', 0)
+            max_value = info.get('max', 1)
+
+            knob = nuke.Double_Knob(k)
+            knob.setRange(min_value, max_value)
+            knob.setDefaultValue([default_value])
+            knob.setTooltip(tooltip)
+
+        elif _class == 'STRING':
+            multiline = info.get('multiline', False)
+
+            if multiline:
+                knob = nuke.Multiline_Eval_String_Knob(k)
+            else:
+                knob = nuke.String_Knob(k)
+
+            knob.setTooltip(tooltip)
+
+        elif type(_class) == list:
+            knob = nuke.Enumeration_Knob(k, k, _class)
+            knob.setTooltip(tooltip)
+
+        else:
+            continue
+
+        n.addKnob(knob)
 
 
 def update():
