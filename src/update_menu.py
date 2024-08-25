5d1356157c4adcceaa89443adc575c02723d695d
Francisco <fcocc77@gmail.com>
2024-08-25 11:43:25 -0400

    randomize tooltip

--- src/update_menu.py
+++ src/update_menu.py
@@ -153,6 +153,8 @@ def create_node(data, inpanel=True):
 
         if 'seed' in key:
             randomize_knob = nuke.Boolean_Knob('randomize')
+            randomize_knob.setTooltip(
+                'Allows the linked QueuePrompt to automatically change the seed by randomizing the number.')
             randomize_knob.setValue(False)
             n.addKnob(randomize_knob)
 
