32485fb9f6802be509e18a1ef714b86e8a7dab7c
Francisco <fcocc77@gmail.com>
2024-08-25 10:12:39 -0400

    workflow_importer: update

--- src/workflow_importer.py
+++ src/workflow_importer.py
@@ -11,6 +11,16 @@ from .nodes import get_node_data
 from .connection import convert_to_utf8
 
 
+def center_nodes(nodes):
+    min_x = min(node['xpos'].value() for node in nodes)
+    min_y = min(node['ypos'].value() for node in nodes)
+
+    for node in nodes:
+        new_x = node['xpos'].value() - min_x
+        new_y = node['ypos'].value() - min_y
+        node.setXYpos(int( new_x ), int( new_y ))
+
+
 def import_workflow():
     workflow_path = nuke.getFilename('Workflow', '*.json')
     if not workflow_path:
@@ -22,17 +32,18 @@ def import_workflow():
     [n.setSelected(False) for n in nuke.selectedNodes()]
 
     create_nodes = {}
+    not_installed = []
 
     for attrs in data['nodes']:
         node = create_comfyui_node(attrs['type'], inpanel=False)
 
-        if node:
-            create_nodes[attrs['id']] = (node, attrs)
-
-        else:
+        if not node:
             node = nuke.createNode('NoOp', inpanel=False)
             node.setName(remove_signs(attrs['type']))
             error_node_style(node.fullName(), True, 'Node not installed !')
+            not_installed.append(attrs['type'])
+
+        create_nodes[attrs['id']] = (node, attrs)
 
         node.setSelected(False)
         xpos, ypos = attrs['pos']
@@ -52,7 +63,12 @@ def import_workflow():
                     return node
 
     for node, attrs in create_nodes.values():
+        node.setSelected(True)
+
         node_data = get_node_data(node)
+        if not node_data:
+            continue
+
         knobs_order = node_data['knobs_order']
 
         for i, value in enumerate(attrs.get('widgets_values', [])):
@@ -67,3 +83,10 @@ def import_workflow():
             link = idata['link']
             onode = find_node_link(link)
             node.setInput(i, onode)
+
+    center_nodes([n[0] for n in create_nodes.values()])
+
+    if not_installed:
+        nodes_list = '\n'.join(not_installed)
+        nuke.message(
+            'You need to install these nodes in ComfyUI:\n\n' + nodes_list)
