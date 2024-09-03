e1f18312a2a4dac6d9202ad1e06600a8df4d5445
Francisco Contreras <fcocc77@gmail.com>
2024-09-02 21:12:32 -0400

    gizmos

--- __init__.py
+++ __init__.py
@@ -20,11 +20,12 @@ def setup():
     comfyui_menu = nuke.menu('Nodes').addMenu('ComfyUI', icon=icon)
 
     icon_gray = '{}/icons/comfyui_icon_gray.png'.format(path)
-    nodes_dir = os.path.join(path, 'nodes', 'ComfyUI')
+    nodes_dir = os.path.join(path, 'nodes')
 
     refresh_icon = '{}/icons/refresh.png'.format(path)
     basic_icon = '{}/icons/basic.png'.format(path)
     workflow_icon = '{}/icons/workflow.png'.format(path)
+    gizmos_icon = '{}/icons/gizmos.png'.format(path)
 
     comfyui_menu.addCommand(
         'Update all ComfyUI', update_menu.update, '', refresh_icon)
@@ -33,14 +34,28 @@ def setup():
         'Import Workflow', workflow_importer.import_workflow, '', workflow_icon)
 
     comfyui_menu.addMenu('Basic Nodes', basic_icon)
+    comfyui_menu.addMenu('Gizmos', gizmos_icon)
 
     def create_node(nk):
         node = nuke.nodePaste(os.path.join(nodes_dir, nk))
         node.showControlPanel()
 
-    for nk in os.listdir(nodes_dir):
-        name = 'Basic Nodes/' + nk.split('.')[0]
-        comfyui_menu.addCommand(name, partial(create_node, nk), '', icon_gray)
+    for dirname in os.listdir(nodes_dir):
+        folder = os.path.join(nodes_dir, dirname)
+
+        if not os.path.isdir(folder):
+            continue
+
+        for nk in os.listdir(folder):
+            if not nk.split('.')[-1] == 'nk':
+                continue
+
+            name = '{}/{}'.format('Basic Nodes' if dirname ==
+                                  'ComfyUI' else dirname, nk.split('.')[0])
+
+            path_nk = os.path.join(folder, nk)
+            comfyui_menu.addCommand(name, partial(
+                create_node, path_nk), '', icon_gray)
 
     if update_menu_at_start:
         update_menu.update()
