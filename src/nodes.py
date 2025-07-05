aa8303de46d320f5f125d325ba6f862d56493b94
Francisco <fcocc77@gmail.com>
2025-07-04 22:49:42 -0400

    input image was rendered twice: fixed

--- src/nodes.py
+++ src/nodes.py
@@ -96,7 +96,7 @@ def create_load_images_and_save(node, alpha, tonemap, frame=-1):
         n.setSelected(False)
         node_state = ''
         for k in n.knobs().values():
-            node_state += str(k.value()) if k.hasExpression() else k.toScript()
+            node_state += str(k.valueAt(0)) if k.hasExpression() else k.toScript()
 
         node_state = node_state.replace(
             str(n.xpos()), '').replace(str(n.ypos()), '')
