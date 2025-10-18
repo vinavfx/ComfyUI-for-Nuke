914a35ea9d8fc43d46cdd169b26e1e388ca3bcd5
pancho <fcocc77@gmail.com>
2025-10-18 19:55:21 -0300

    settings: update

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 import os
 
 # ENVIRONMENT VARIABLES
-COMFYUI_DIR = os.getenv('NUKE_COMFYUI_DIR', '<Put the ComfyUI path here>')
-IP = os.getenv('NUKE_COMFYUI_IP', '127.0.0.1')
-PORT = int(os.getenv('NUKE_COMFYUI_PORT', '8188'))
-COMFYUI2NUKE = os.path.dirname(__file__)
-IMAGE_OUTPUT_WITHIN_PROJECT = os.getenv('IMAGE_OUTPUT_WITHIN_PROJECT', False)
-COMFYUI_LOCAL = os.getenv('COMFYUI_LOCAL', True)
+COMFYUI_DIR =                       os.getenv('NUKE_COMFYUI_DIR', '<Put the ComfyUI path here>')
+IP =                                os.getenv('NUKE_COMFYUI_IP', '127.0.0.1')
+PORT =                              int(os.getenv('NUKE_COMFYUI_PORT', '8188'))
+COMFYUI2NUKE =                      os.path.dirname(__file__)
+IMAGE_OUTPUT_WITHIN_PROJECT =       bool(int(os.getenv('IMAGE_OUTPUT_WITHIN_PROJECT', '0')))
+COMFYUI_LOCAL =                     bool(int(os.getenv('COMFYUI_LOCAL', '1')))
 
 # SETTINGS
 UPDATE_MENU_AT_START = False
