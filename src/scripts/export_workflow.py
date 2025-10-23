9c1ad6385ff2f80b221d48c9187ddc5529eaef3c
pancho <fcocc77@gmail.com>
2025-10-23 19:30:51 -0300

    export_workflow: update

--- src/scripts/export_workflow.py
+++ src/scripts/export_workflow.py
@@ -8,6 +8,7 @@ import os
 from ..nodes import extract_data
 from ...nuke_util.nuke_util import selected_node
 from ...python_util.util import jwrite
+from ..common import update_images_and_mask_inputs
 
 
 def export_workflow():
@@ -15,16 +16,31 @@ def export_workflow():
     if not node:
         return
 
-    if not node.knob('comfyui_submit'):
+    if node.knob('comfyui_gizmo'):
+        node.begin()
+        node = nuke.toNode('Run')
+
+    elif not node.knob('comfyui_submit'):
         nuke.message("Select the 'Run' node")
         return
 
+    update_images_and_mask_inputs()
+    data, _ = extract_data(node)
+
+    if not data:
+        return
+
     workflow = nuke.getFilename(
-        'Export Workflow', "*.json", os.path.join(os.path.expanduser('~'), 'Desktop/workflow'))
+        'Export Workflow',
+        "*.json",
+        os.path.join(os.path.expanduser('~'), 'Desktop/workflow.json'),
+        type='save'
+    )
 
     if not workflow:
         return
 
     workflow = workflow if 'json' in workflow else workflow + '.json'
-    data, _ = extract_data(node)
     jwrite(workflow, data)
+
+    nuke.message('Workflow Saved: ' + workflow)
