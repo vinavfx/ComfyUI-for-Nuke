3d7700748466020e13c23ca30a43e63d771a6465
Francisco Contreras <fcocc77@gmail.com>
2026-01-15 11:50:50 -0300

    settings: default url fixed

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 # ENVIRONMENT VARIABLES
 COMFYUI_DIR =                       os.getenv('NUKE_COMFYUI_DIR', '')
-URL =                               os.getenv('NUKE_COMFYUI_URL', '0.0.0.0:8188')
+URL =                               os.getenv('NUKE_COMFYUI_URL', '127.0.0.1:8188')
 COMFYUI2NUKE =                      os.path.dirname(__file__)
 COMFYUI_LOCAL =                     bool(int(os.getenv('COMFYUI_LOCAL', '1')))
 OUTPUT_DIRECTORY =                  os.getenv('OUTPUT_DIRECTORY', '')
