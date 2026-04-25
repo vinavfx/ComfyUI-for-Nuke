33b18bb40ab82d815cd54fece1411c81d3bcbb15
Francisco Contreras <fcocc77@gmail.com>
2026-04-25 16:21:20 -0400

    knob2input: get_swapped_knobs

--- src/scripts/knob2input.py
+++ src/scripts/knob2input.py
@@ -8,55 +8,72 @@ from ...nuke_util.nuke_util import selected_node
 from ..nodes import get_node_data, save_node_data
 
 
+def get_swapped_knobs(node):
+    data = get_node_data(node)
+    if not data:
+        nuke.message('Must be a ComfyUI node !')
+        return None, None
+
+    swapped_knobs = {}
+
+    for nuke_knob_name in data['knobs_order']:
+        if not nuke_knob_name in data['knobs_class']:
+            continue
+
+        knob_name = nuke_knob_name[:-1]
+
+        if any(v.get('name') == knob_name for v in data['inputs']):
+            swapped_knob = True
+        else:
+            swapped_knob = False
+
+        swapped_knobs[knob_name] = {
+            'class': data['knobs_class'][nuke_knob_name],
+            'swapped_knob': swapped_knob
+        }
+
+    return swapped_knobs, data
+
+
 def knob_to_input():
     node = selected_node()
     if not node:
         return
 
-    data = get_node_data(node)
-    if not data:
-        nuke.message('Must be a ComfyUI node !')
-        return
-
     panel = nuke.Panel('Knob to Input ({})'.format(node.name()))
 
-    knobs = []
-    for knob in data['knobs_order']:
-        if not knob in data['knobs_class']:
-            continue
-        _class = data['knobs_class'][knob]
-        knobs.append((knob, knob[:-1], _class))
+    swapped_knobs, data = get_swapped_knobs(node)
+    if not swapped_knobs:
+        return
 
-    for knob, knob_name, _ in knobs:
-        if any(v.get('name') == knob_name for v in data['inputs']):
-            panel.addBooleanCheckBox(knob_name, True)
-        else:
-            panel.addBooleanCheckBox(knob_name, False)
+    for knob_name, knob in swapped_knobs.items():
+        panel.addBooleanCheckBox(knob_name, knob['swapped_knob'])
 
     if not panel.show():
         return
 
-    knobs = [[panel.value(knob_name), knob, knob_name, _class]
-             for knob, knob_name, _class in knobs]
+    for knob_name, knob in swapped_knobs.items():
+        swapped_knobs[knob_name]['swapped_knob'] = panel.value(knob_name)
 
-    convert_knobs(node, data, knobs)
+    convert_knobs(node, data, swapped_knobs)
 
 
-def convert_knobs(node, data, knobs):
-    for value, knob, knob_name, _class in knobs:
+def convert_knobs(node, data, swapped_knobs):
+    for knob_name, knob in swapped_knobs.items():
+        nuke_knob_name = knob_name + '_'
 
         knob_data = {
             'opt': False,
-            'outputs': [_class],
+            'outputs': [knob['class']],
             'name': knob_name
         }
 
         exists_konb = any(i.get('name') == knob_name for i in data['inputs'])
 
-        if value:
+        if knob['swapped_knob']:
             if not exists_konb:
                 data['inputs'].append(knob_data)
-                _knob = node.knob(knob)
+                _knob = node.knob(nuke_knob_name)
                 if _knob is None:
                     continue
                 _knob.setName(knob_name + '_hide')
@@ -77,7 +94,7 @@ def convert_knobs(node, data, knobs):
             _knob = node.knob(knob_name + '_hide')
             if _knob is None:
                 continue
-            _knob.setName(knob)
+            _knob.setName(nuke_knob_name)
             _knob.setVisible(True)
             node.knob('label').setValue('')
 
